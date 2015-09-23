9fb40fd4e865c808ee7913b614d0d4bf4a4ace69
abraham28s <ab.soto@hotmail.com>
2015-09-23 10:59:00 -0500

    Update GeneradorEne.cs

--- GeneradorEne.cs
+++ GeneradorEne.cs
@@ -4,15 +4,20 @@ using System.Collections;
 public class GeneradorEne : MonoBehaviour {
 
 	public GameObject[] obj;
-	public float tiempoMin = 1f;
-	public float tiempoMax = 2f;
+	public float tiempoMin = 4f;
+	public float tiempoMax = 6f;
 	// Use this for initialization
 	void Start () {
-	
+		Generar ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+	}
+
+	void Generar(){
 		Instantiate (obj [Random.Range (0, obj.Length)], transform.position, Quaternion.identity);
+		Invoke ("Generar", Random.Range (tiempoMin, tiempoMax));
 	}
 }
